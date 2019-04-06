fc9e7b89ad62414c9a7cdd4da4e6893b8be91719
unknown <jonathancaballero@uninorte.edu.co>
2019-04-05 23:01:36 -0500

    Lee el valor de x

--- Laboratorio2-Datos2/src/laboratorio2/datos2/Laboratorio2Datos2.java
+++ Laboratorio2-Datos2/src/laboratorio2/datos2/Laboratorio2Datos2.java
@@ -1,21 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package laboratorio2.datos2;
 
-/**
- *
- * @author JONATHAN
- */
+import java.util.Scanner;
+
+
 public class Laboratorio2Datos2 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        int x;
+        Scanner SC = new Scanner (System.in);
+        System.out.println("Lean x");
+        x = SC.nextInt();
     }
     
 }
